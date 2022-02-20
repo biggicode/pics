abb1d0f29b01f23e7f54a0198076822429f969b7
biggicode <larioncescu@yahoo.com>
2022-02-20 18:12:47 +0200

    refactor app

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,17 +1,14 @@
 import React from "react"
-import axios from "axios"
+import unsplash from "../../api/unsplash"
 import { SearchBar } from "../search-bar"
 
 export class App extends React.Component {
   state = { images: [] }
 
   handleSearchSubmit = async (term) => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await unsplash.get("/search/photos", {
       params: {
         query: term,
-      },
-      headers: {
-        Authorization: "Client-ID _sXPJXHuGY4BF3gPiyja4TByYbUKoSMCYTwtO3tPt6c",
       },
     })
 
