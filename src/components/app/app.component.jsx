8d500f5b6b3b6a3910812455adf918005e6b1b93
biggicode <larioncescu@yahoo.com>
2022-02-20 17:46:29 +0200

    make handle submit method async

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import axios from "axios"
 import { SearchBar } from "../search-bar"
 
 export class App extends React.Component {
-  handleSearchSubmit(term) {
-    console.log(term)
+  async handleSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID _sXPJXHuGY4BF3gPiyja4TByYbUKoSMCYTwtO3tPt6c",
+      },
+    })
   }
 
   render() {
